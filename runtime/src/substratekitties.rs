9ceef5824cbcdcbb8fd2b2f0eab5e3cdd4bac6ad
Ozaki Yoichi <zak@nli.ics.keio.ac.jp>
2019-02-13 12:57:26 +0900

    add strorage mapping: AccountId => u64wq

--- runtime/src/substratekitties.rs
+++ runtime/src/substratekitties.rs
@@ -1,12 +1,12 @@
 use system::ensure_signed;
-use support::{decl_storage, decl_module, StorageValue, dispatch::Result};
+use support::{decl_storage, decl_module, StorageMap, dispatch::Result};
 
 pub trait Trait: system::Trait {}
 
 decl_storage! {
     trait Store for Module<T: Trait> as KittyStorage {
         // Declare storage and its getter functions here.
-        Value: u64;
+        Value: map T::AccountId => u64;
     }
 }
 
@@ -14,8 +14,8 @@ decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
         // Declare public functions here.
         fn set_value(origin, value: u64) -> Result {
-            let _sender = ensure_signed(origin)?;
-            <Value<T>>::put(value);
+            let sender = ensure_signed(origin)?;
+            <Value<T>>::insert(sender, value);
             Ok(())
         }
     }
