6801ba8b6ea3125ffd3d2bfc3657e83e59ad4dce
Ozaki Yoichi <zak@nli.ics.keio.ac.jp>
2019-02-14 11:19:52 +0900

    add map of account to kitty structure and its interface

--- runtime/src/substratekitties.rs
+++ runtime/src/substratekitties.rs
@@ -11,12 +11,24 @@ pub struct Kitty<Hash, Balance> {
     gen: u64,
 }
 
-// Substrateでは「あるトランザクショううがFinalizeされたことが、すなわちそのトランザクションによって実行される
-// 関数が成功裏に終わったことを意味しない」。Substrateでは「呼び出された関数が成功裏に終わったこと」を
+// Substrateでは「あるトランザクショううがFinalizeされたことが、直接そのトランザクションによって実行される
+// 関数が成功裏に終わったこと」を意味しない。Substrateでは「呼び出された関数が成功裏に終わったこと」を
 // Eventというものを明示的に返すことで表現する。Eventには任意の型を与えることができる。
 // Eventの役割は「その関数の実行の成否を報告すること」と
 // 「Off-chainの世界に、ブロックチェーン上で状態遷移が発生したことを宣言すること」である。
 // Eventの定義にはdecl_eventマクロを使うと簡単にできるようになっている。
+
+// Ethereum上のコントラクトを開発するのではなく、substrateでチェーンのロジックを開発するのだから、
+// ブロックチェーンの状態を変化させうるあらゆる事柄に対して注意を払わないといけない。
+// substrateではリスト型をプリミティブな型として提供していない。
+// 何故ならばリスト型は予期せず危険な動作を引き起こす可能性があるからである。
+// 例えば「リストの要素を一つずつイテレーションしていく」という操作は、最悪の場合O(n)の計算量を必要とすることに
+// なりかねない。
+// そこでsubstrate上でリストのようなデータ構造を実現したいならば、マッピングを利用して実装する必要がある。
+// リストの操作は注意が必要である。具体的にはoverflow/underflowしないように注意する。幸いなことに、
+// rustは型安全な演算が言語機能として提供されている。もしくはrustのResult型を使うことでも対応できる。
+// Verify first, write lastの原則：安全な操作であることを確認してから、ブロックに書き込む。
+
 pub trait Trait: balances::Trait {
     type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 }
@@ -27,18 +39,23 @@ decl_event!(
     }
 );
 
-// decl_strorageマクロの適用
+// decl_storageマクロの適用によってチェーンに刻むデータ構造を定義する。
 decl_storage! {
     trait Store for Module<T: Trait> as KittyStorage {
         // Declare storage and its getter functions here.
+
+        // hash value is a unique key to each kitty.
         Kitties get(kitty): map T::Hash => Kitty<T::Hash, T::Balance>; // hash value => kitty
         KittyOwner get(owner_of): map T::Hash => Option<T::AccountId>; // hash value => account ID
         OwnedKitty get(kitty_of_owner): map T::AccountId => T::Hash;   // account ID => hash value
+        AllKittiesArray get(kitty_by_index): map u64 => T::Hash;       // kitty's index => hash value
+        AllKittiesCount get(all_kitties_count): u64;                   // how many kitties exist?
+        AllKittiesIndex: map T::Hash => u64;                           // hash value => kitty's index
 
         Nonce: u64;
     }
 }
-// decl_moduleマクロの適用
+// decl_moduleマクロの適用によってチェーンに刻むデータへのアクセスインタフェースの実装を記述する。
 decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
         // Declare public functions here.
@@ -47,9 +64,17 @@ decl_module! {
         fn deposit_event<T>() = default;
 
         // 新しいKittyを生成し、その成否を返す関数を定義する。
+        // Kittyたちはリストのような見た目のデータ構造でアカウントに紐づけられた形で管理される。
         fn create_kitty(origin) -> Result {
-            // create_kitty()を叩いたsenderの正当性を確認する。
+            // Verify first, write lastの原則：create_kitty()を叩いたsenderの正当性を確認する。
             let sender = ensure_signed(origin)?;
+
+            // Verify first, write lastの原則：現在登録されているkittiesの個体数を確認する。
+            let all_kitties_count = Self::all_kitties_count();
+
+            // Verify first, write lastの原則：これから登録しようとしているkittyを追加してoverflowしないかを確認する。
+            let new_all_kitties_count = all_kitties_count.checked_add(1)
+                                            .ok_or("Error: Overflow happened when trying to  register a new kitty")?;
 
             // nonceを計算する。
             let nonce = <Nonce<T>>::get();
@@ -75,6 +100,15 @@ decl_module! {
 
             // (生成者を一意に区別するハッシュ値, 生成者)を登録する。
             <KittyOwner<T>>::insert(random_hash, &sender);
+
+            // (all_kitties_count, random_hash)を登録する。all_kitties_countは0オリジンの通し番号となる。
+            <AllKittiesArray<T>>::insert(all_kitties_count, random_hash);
+
+            // 「現在のkittiesの個体数」を更新
+            <AllKittiesCount<T>>::put(new_all_kitties_count);
+
+            // (random_hash, all_kitties_count)を登録する。
+            <AllKittiesIndex<T>>::insert(random_hash, all_kitties_count);
 
             // (生成者, 生成者を一意に区別するハッシュ値)を登録する。
             <OwnedKitty<T>>::insert(&sender, random_hash);
